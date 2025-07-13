b78cecec86798aa2708087277c66d0cbc39d3b98
digihori <yo6987@gmail.com>
2025-07-13 17:43:16 +0900

    細かな修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "tk.horiuchi.beamgalaxian"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
